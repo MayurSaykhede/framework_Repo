acfd92c4df110ac45a6341e1b1c6a77e0b595e7f
MayurSaykhede <167409893+MayurSaykhede@users.noreply.github.com>
2024-11-12 16:10:52 +0530

    1st Commit

---
+++ Jenkins_Practice/src/test/java/sample/sampleClass1_Test.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sampleClass1_Test {
+	
+	@Test
+	public void sample1Test() {
+		System.out.println("sample1Test()");
+	}
+
+}
---
+++ Jenkins_Practice/src/test/java/sample/sampleClass2_Test.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sampleClass2_Test {
+	
+	@Test
+	public void sample2Test() {
+		System.out.println("sample2Test()");
+	}
+	
+	@Test
+	public void sample3Test() {
+		System.out.println("sample3Test()");
+	}
+}
