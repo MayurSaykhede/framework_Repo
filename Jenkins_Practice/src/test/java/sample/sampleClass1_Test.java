da369adfe0f1d43db4e9a81153da4347a2d22fbb
MayurSaykhede <167409893+MayurSaykhede@users.noreply.github.com>
2024-11-12 17:31:44 +0530

    Added code to fetch Jenkins parameter

--- Jenkins_Practice/src/test/java/sample/sampleClass1_Test.java
+++ Jenkins_Practice/src/test/java/sample/sampleClass1_Test.java
@@ -3,10 +3,22 @@ package sample;
 import org.testng.annotations.Test;
 
 public class sampleClass1_Test {
-	
+
 	@Test
 	public void sample1Test() {
 		System.out.println("sample1Test()");
 	}
 
+	@Test
+	public void fethchingJenkinsPrameter() {
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
 }
